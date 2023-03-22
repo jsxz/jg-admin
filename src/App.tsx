73cd941333f9d4e89420357c35e405229b950683
anjunact <anjunact@qq.com>
2023-03-22 23:06:12 +0800

    新方式加路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import {useState} from 'react'
 import {Button} from 'antd'
 import {UpCircleOutlined} from "@ant-design/icons"
-import {Link, Outlet} from "react-router-dom";
+import {Link, Outlet, useRoutes} from "react-router-dom";
+import router from "@/router";
 
 // import 'antd/dist/reset.css'
 function App() {
-
+    const outlet=useRoutes(router)
     return (
         <div className="App">
             <Link to="/home">Home</Link>|
             <Link to="/about">About</Link>
-            <Outlet></Outlet>
+            {/*<Outlet></Outlet>*/}
+            {outlet}
         </div>
     )
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,20 @@
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import App from "@/App";
+import * as path from "path";
 import Home from "@/views/Home";
 import About from "@/views/About";
+import {Navigate} from "react-router-dom";
 
-const baseRouter = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App/>}>
-                {/*/重定向到/home*/}
-                <Route path="/" element={<Navigate to="/home"/>}></Route>
-                <Route path="/home" element={<Home/>}></Route>
-                <Route path="/about" element={<About/>}></Route>
-            </Route>
-        </Routes>
-    </BrowserRouter>
-)
-export default baseRouter
+const routes=[
+    {
+        path:"/",
+        element:<Navigate to="/home"/>
+    },
+    {
+        path:"/home",
+        element:<Home/>
+    },
+    {
+        path:"/about",
+        element:<About/>
+    },
+]
+export default routes
